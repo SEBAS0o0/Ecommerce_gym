cae35a221328698a7436c9ff2c2d2e09ea1a1ab1
Steven0o0 <92197217+SEBAS0o0@users.noreply.github.com>
2023-12-20 22:49:46 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Simula una carga asíncrona
+    setTimeout(function() {
+        // Oculta el preloader
+        document.querySelector('.preloader').style.display = 'none';
+        // Muestra el contenido principal
+        document.getElementById('main-content').style.display = 'block';
+    }, 4000); // Puedes ajustar el tiempo de simulación de carga
+});
