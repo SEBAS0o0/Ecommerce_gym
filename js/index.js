7444bc1855083e27c6b0b6149c8fa8d062648e50
Steven0o0 <92197217+SEBAS0o0@users.noreply.github.com>
2024-01-11 21:41:35 -0500

    Delete js directory

--- js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simula una carga asíncrona
-    setTimeout(function() {
-        // Oculta el preloader
-        document.querySelector('.preloader').style.display = 'none';
-    }, 2000); // Puedes ajustar el tiempo de simulación de carga
-});
